b3716ded0714f00583b5aa5bfe86aa090c670302
Craig Duffin <craig.duffin@sky.uk>
2017-06-26 15:13:16 +0100

    added work in progress of traffic IP group

--- brocadevtm/provider.go
+++ brocadevtm/provider.go
@@ -46,8 +46,9 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"brocadevtm_monitor": resourceMonitor(),
-			"brocadevtm_pool":    resourcePool(),
+			"brocadevtm_monitor":          resourceMonitor(),
+			"brocadevtm_pool":             resourcePool(),
+			"brocadevtm_traffic_ip_group": resourceTrafficIPGroup(),
 		},
 
 		ConfigureFunc: providerConfigure,
