1a5baa4f5bcdbd5464ec8ee7ab08764630b6f11d
Paul Warwick <paul.warwick@sky.uk>
2017-07-11 18:03:16 +0100

    Implemented CRUD with three attributes and extended acceptance tests

--- brocadevtm/resource_virtual_server.go
+++ brocadevtm/resource_virtual_server.go
@@ -3,6 +3,11 @@ package brocadevtm
 import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"fmt"
+	"regexp"
+	"github.com/sky-uk/go-brocade-vtm"
+	"github.com/sky-uk/go-brocade-vtm/api/virtualserver"
+	"net/http"
+	"encoding/json"
 )
 
 func resourceVirtualServer() *schema.Resource {
@@ -18,10 +23,22 @@ func resourceVirtualServer() *schema.Resource {
 				Description: "Name of the virtual server",
 				Required: true,
 			},
+			"enabled": {
+				Type: schema.TypeBool,
+				Description: "Whether the virtual server should be enabled",
+				Optional: true,
+				Computed: true,
+			},
+			"listen_on_any": {
+				Type: schema.TypeBool,
+				Description: "Whether the virtual server should listen on any",
+				Optional: true,
+				Computed: true,
+			},
 			"listen_traffic_ips": {
 				Type:        schema.TypeList,
 				Description: "List of traffic IPs to listen on",
-				Required:    true,
+				Optional: true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 			"pool": {
@@ -38,7 +55,9 @@ func resourceVirtualServer() *schema.Resource {
 			"protocol": {
 				Type: schema.TypeString,
 				Description: "Protocol to use with the virtual server",
-				Required: true,
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerProtocol,
 			},
 			"request_rules": {
 				Type: schema.TypeList,
@@ -49,17 +68,109 @@ func resourceVirtualServer() *schema.Resource {
 				Type: schema.TypeBool,
 				Description: "Whether to enable or disable SSL",
 				Optional: true,
+				Computed: true,
 			},
 			"connection_keepalive": {
 				Type: schema.TypeBool,
-				Description: "Whether to enable keepalive",
+				Description: "Whether to enable keepalive for remote clients",
 				Optional: true,
+				Computed: true,
 			},
 			"connection_keepalive_timeout": {
 				Type: schema.TypeInt,
-				Description: "Keepalive timeout",
+				Description: "Keepalive timeout for idle connections",
 				Optional: true,
+				Computed: true,
 				ValidateFunc: validateVirtualServerUnsignedInteger,
+			},
+			"connection_max_client_buffer": {
+				Type: schema.TypeInt,
+				Description: "Max memory in bytes for stored client data",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUnsignedInteger,
+			},
+			"connection_max_server_buffer": {
+				Type: schema.TypeInt,
+				Description: "Max memory in bytes for stored server data",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUnsignedInteger,
+			},
+			"connection_max_transaction_duration": {
+				Type: schema.TypeInt,
+				Description: "Max amount of time a transaction can take",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUnsignedInteger,
+			},
+			"connection_server_first_banner": {
+				Type: schema.TypeString,
+				Description: "Banner to send for server first protocols",
+				Optional: true,
+			},
+			"connection_timeout": {
+				Type: schema.TypeInt,
+				Description: "Time to wait before closing a connection when no additional data has been sent",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUnsignedInteger,
+			},
+			"ssl_server_cert_default": {
+				Type: schema.TypeString,
+				Description: "Default SSL certificate",
+				Optional: true,
+			},
+			"ssl_server_cert_host_mapping_host": {
+				Type: schema.TypeString,
+				Description: "Which host the SSL certificate refers to",
+				Optional: true,
+			},
+			"ssl_server_cert_host_mapping_alt_certificates": {
+				Type: schema.TypeList,
+				Description: "SSL server certificates for a particular destination IP",
+				Optional: true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			"ssl_server_cert_host_mapping_certificate": {
+				Type: schema.TypeString,
+				Description: "The SSL server certificate for a particular destination",
+				Optional: true,
+			},
+			"ssl_support_ssl2": {
+				Type: schema.TypeString,
+				Description: "Whether or not SSLv2 is enabled for this virtual server",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUseSSLSupport,
+			},
+			"ssl_support_ssl3": {
+				Type: schema.TypeString,
+				Description: "Whether or not SSLv3 is enabled for this virtual server",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUseSSLSupport,
+			},
+			"ssl_support_tls1": {
+				Type: schema.TypeString,
+				Description: "Whether or not TLSv1.0 is enabled for this virtual server",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUseSSLSupport,
+			},
+			"ssl_support_tls1_1": {
+				Type: schema.TypeString,
+				Description: "Whether or not TLSv1.1 is enabled for this virtual server",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUseSSLSupport,
+			},
+			"ssl_support_tls1_2": {
+				Type: schema.TypeString,
+				Description: "Whether or not TLSv1.2 is enabled for this virtual server",
+				Optional: true,
+				Computed: true,
+				ValidateFunc: validateVirtualServerUseSSLSupport,
 			},
 		},
 	}
@@ -73,18 +184,155 @@ func validateVirtualServerUnsignedInteger(v interface{}, k string) (ws []string,
 	return
 }
 
+func validateVirtualServerProtocol(v interface{}, k string) (ws []string, errors []error) {
+	protocol := v.(string)
+	protocolOptions := regexp.MustCompile(`^(client_first|dns|dns_tcp|ftp|http|https|imaps|imapv2|imapv3|imapv4|ldap|ldaps|pop3|pop3s|rtsp|server_first|siptcp|sipudp|smtp|ssl|stream|telnet|udp|udpstreaming)$`)
+	if !protocolOptions.MatchString(protocol) {
+		errors = append(errors, fmt.Errorf("%q must be one of client_first, dns, dns_tcp, ftp, http, https, imaps, imapv2, imapv3, imapv4, ldap, ldaps, pop3, pop3s, rtsp, server_first, siptcp, sipudp, smtp, ssl, stream, telnet, udp or udpstreaming", k))
+	}
+	return
+}
+
+func validateVirtualServerUseSSLSupport(v interface{}, k string) (ws []string, errors []error) {
+	sslUseSSLSupport := v.(string)
+	sslUseSSLSupportOptions := regexp.MustCompile(`^(use_default|disabled|enabled)$`)
+	if !sslUseSSLSupportOptions.MatchString(sslUseSSLSupport) {
+		errors = append(errors, fmt.Errorf("%q must be one of use_default, disabled or enabled", k))
+	}
+	return
+}
+
 func resourceVirtualServerCreate(d *schema.ResourceData, m interface{}) error {
-	return nil
+
+	vtmClient := m.(*brocadevtm.VTMClient)
+	var virtualServerName string
+	var virtualServer virtualserver.VirtualServer
+
+	if v, ok := d.GetOk("name"); ok && v != "" {
+		virtualServerName = v.(string)
+	}
+	if v, ok := d.GetOk("pool"); ok && v != "" {
+		virtualServer.Properties.Basic.Pool = v.(string)
+	}
+	if v, ok := d.GetOk("port"); ok && v != "" {
+		virtualServerPort := v.(int)
+		virtualServer.Properties.Basic.Port = uint(virtualServerPort)
+	}
+
+	createAPI := virtualserver.NewCreate(virtualServerName, virtualServer)
+	err := vtmClient.Do(createAPI)
+	if err != nil {
+		return fmt.Errorf("Brocade vTM Virtual Server Create failed for %s with error: %+v", virtualServerName, err)
+	}
+	if createAPI.StatusCode() != http.StatusCreated {
+		return fmt.Errorf("Brocade vTM Virtual Server Create failed for %s with http status code != 201", virtualServerName)
+	}
+	d.SetId(virtualServerName)
+	return resourceVirtualServerRead(d, m)
 }
 
 func resourceVirtualServerRead(d *schema.ResourceData, m interface{}) error {
+
+	vtmClient := m.(*brocadevtm.VTMClient)
+	var virtualServerName string
+	var virtualServer virtualserver.VirtualServer
+
+	if v, ok := d.GetOk("name"); ok && v != "" {
+		virtualServerName = v.(string)
+	}
+
+	getSingleAPI := virtualserver.NewGetSingle(virtualServerName)
+	err := vtmClient.Do(getSingleAPI)
+	if err != nil {
+		return fmt.Errorf("Brocade vTM Virtual Server Read failed for %s with error: %+v", virtualServerName, err)
+	}
+	if getSingleAPI.StatusCode() == http.StatusNotFound {
+		d.SetId("")
+		return nil
+	}
+
+	virtualServer = *getSingleAPI.GetResponse()
+	d.SetId(virtualServerName)
+	d.Set("pool", virtualServer.Properties.Basic.Pool)
+	d.Set("port", virtualServer.Properties.Basic.Port)
 	return nil
 }
 
 func resourceVirtualServerUpdate(d *schema.ResourceData, m interface{}) error {
-	return nil
+
+	vtmClient := m.(*brocadevtm.VTMClient)
+	var virtualServerName string
+	var virtualServer, responseVirtualServer virtualserver.VirtualServer
+	hasChanges := false
+
+	if v, ok := d.GetOk("name"); ok && v != "" {
+		virtualServerName = v.(string)
+	}
+	if d.HasChange("pool") {
+		if v, ok := d.GetOk("pool"); ok && v != "" {
+			virtualServer.Properties.Basic.Pool = v.(string)
+		}
+		hasChanges = true
+	}
+	if d.HasChange("port") {
+		if v, ok := d.GetOk("port"); ok && v != "" {
+			virtualServerPort := v.(int)
+			virtualServer.Properties.Basic.Port = uint(virtualServerPort)
+		}
+		hasChanges = true
+	}
+
+	if hasChanges {
+		updateAPI := virtualserver.NewUpdate(virtualServerName, virtualServer)
+		err := vtmClient.Do(updateAPI)
+		if err != nil {
+			return fmt.Errorf("Brocade vTM Virtual Server update failed for %s", virtualServerName)
+		}
+		responseCode := updateAPI.StatusCode()
+		if responseCode != http.StatusOK {
+			return fmt.Errorf("Brocade vTM Virtual Server update failed for %s with invalid response code %d", virtualServerName, responseCode)
+		}
+
+		response := updateAPI.GetResponse()
+		jsonErr := json.Unmarshal(response, &responseVirtualServer)
+		if jsonErr != nil {
+			return fmt.Errorf("Brocade vTM Virtual Server update faild for %s while unmarshalling JSON response - raw response: %s", virtualServerName, response)
+		}
+
+		d.SetId(virtualServerName)
+		d.Set("pool", responseVirtualServer.Properties.Basic.Pool)
+		d.Set("port", responseVirtualServer.Properties.Basic.Port)
+	}
+	return resourceVirtualServerRead(d, m)
 }
 
 func resourceVirtualServerDelete(d *schema.ResourceData, m interface{}) error {
+
+	vtmClient := m.(*brocadevtm.VTMClient)
+	var virtualServerName string
+
+	if v, ok := d.GetOk("name"); ok && v != "" {
+		virtualServerName = v.(string)
+	}
+	getVirtualServer := virtualserver.NewGetSingle(virtualServerName)
+	err := vtmClient.Do(getVirtualServer)
+	if err != nil {
+		return fmt.Errorf("Brocade vTM Virtual Server delete failed for %s - error: %+v", virtualServerName, err)
+	}
+	if getVirtualServer.StatusCode() == http.StatusNotFound {
+		d.SetId("")
+		return nil
+	}
+	deleteAPI := virtualserver.NewDelete(virtualServerName)
+	err = vtmClient.Do(deleteAPI)
+	if err != nil {
+		return fmt.Errorf("Brocade vTM Virtual Server delete failed for %s - error: %+v", virtualServerName, err)
+	}
+	responseCode := deleteAPI.StatusCode()
+	if responseCode != http.StatusNoContent {
+		return fmt.Errorf("Brocade vTM Virtual Server delete returned an invalid http response code %d for %s", responseCode, virtualServerName)
+	}
+
+	d.SetId("")
 	return nil
 }
--- brocadevtm/resource_virtual_server_test.go
+++ brocadevtm/resource_virtual_server_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"testing"
+	"github.com/sky-uk/go-brocade-vtm"
+	"github.com/sky-uk/go-brocade-vtm/api/virtualserver"
+	"regexp"
 )
 
 func TestAccBrocadeVTMVirtualServerBasic(t *testing.T) {
@@ -27,6 +30,14 @@ func TestAccBrocadeVTMVirtualServerBasic(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
+				Config: testAccBrocadeVTMVirtualServerInvalidInt(virtualServerName),
+				ExpectError: regexp.MustCompile(`can't be negative`),
+			},
+			{
+				Config: testAccBrocadeVTMVirtualServerInvalidProtocol(virtualServerName),
+				ExpectError: regexp.MustCompile(`fsdfdsfsdfsafasdf`),
+			},
+			{
 				Config: testAccBrocadeVTMVirtualServerCreate(virtualServerName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccBrocadeVTMVirtualServerExists(virtualServerName, virtualServerResourceName),
@@ -38,19 +49,148 @@ func TestAccBrocadeVTMVirtualServerBasic(t *testing.T) {
 }
 
 func testAccBrocadeVTMVirtualServerCheckDestroy(state *terraform.State, name string) error {
+
+	vtmClient := testAccProvider.Meta().(*brocadevtm.VTMClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "brocadevtm_virtual_server" {
+			continue
+		}
+		if id, ok := rs.Primary.Attributes["id"]; ok && id != "" {
+			return nil
+		}
+		api := virtualserver.NewGetAll()
+		err := vtmClient.Do(api)
+		if err != nil {
+			return nil
+		}
+		for _, virtual_server := range api.GetResponse().Children {
+			if virtual_server.Name == name {
+				return fmt.Errorf("Brocade vTM Virtual Server %s still exists", name)
+			}
+		}
+	}
 	return nil
 }
 
 func testAccBrocadeVTMVirtualServerExists(virtualServerName, virtualServerResourceName string) resource.TestCheckFunc {
 	return func(state *terraform.State) error {
-		return nil
+
+		rs, ok := state.RootModule().Resources[virtualServerResourceName]
+		if !ok {
+			return fmt.Errorf("\nBrocade vTM Virtual Server %s wasn't found in resources", virtualServerName)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("\nBrocade vTM Virtual Server ID not set for %s in resources", virtualServerName)
+		}
+
+		vtmClient := testAccProvider.Meta().(*brocadevtm.VTMClient)
+		api := virtualserver.NewGetAll()
+		err := vtmClient.Do(api)
+		if err != nil {
+			return fmt.Errorf("Error: %+v", err)
+		}
+		for _, virtualServer := range api.GetResponse().Children {
+			if virtualServer.Name == virtualServerName {
+				return nil
+			}
+		}
+		return fmt.Errorf("Brocade vTM Virtual Server %s not found on remote vTM", virtualServerName)
 	}
+}
+
+func testAccBrocadeVTMVirtualServerInvalidInt(virtualServerName string) string {
+	return fmt.Sprintf(`
+resource "brocadevtm_virtual_server" "acctest" {
+name = "%s"
+enabled = true
+listen_on_any = false
+listen_traffic_ips = ["test-traffic-ip-group"]
+pool = "test-pool"
+port = -80
+protocol = "http"
+request_rules = []
+ssl_decrypt = false
+connection_keepalive = false
+connection_keepalive_timeout = 60
+connection_max_client_buffer = 256
+connection_max_server_buffer = 256
+connection_max_transaction_duration = 100
+connection_server_first_banner = "Banner"
+connection_timeout = 60
+ssl_server_cert_default = "abcdefg"
+ssl_server_cert_host_mapping_host = "test-host.example.com"
+ssl_server_cert_host_mapping_alt_certificates = ["cert1","cert2"]
+ssl_server_cert_host_mapping_certificate = "test-host.example.com"
+ssl_support_ssl2 = "use_default"
+ssl_support_ssl3 = "use_default"
+ssl_support_tls1 = "use_default"
+ssl_support_tls1_1 = "use_default"
+ssl_support_tls1_2 = "use_default"
+}
+`, virtualServerName)
+}
+
+func testAccBrocadeVTMVirtualServerInvalidProtocol(virtualServerName string) string {
+	return fmt.Sprintf(`
+resource "brocadevtm_virtual_server" "acctest" {
+name = "%s"
+enabled = true
+listen_on_any = false
+listen_traffic_ips = ["test-traffic-ip-group"]
+pool = "test-pool"
+port = -80
+protocol = "SOME_INVALID_PROTOCOL"
+request_rules = []
+ssl_decrypt = false
+connection_keepalive = false
+connection_keepalive_timeout = 60
+connection_max_client_buffer = 256
+connection_max_server_buffer = 256
+connection_max_transaction_duration = 100
+connection_server_first_banner = "Banner"
+connection_timeout = 60
+ssl_server_cert_default = "abcdefg"
+ssl_server_cert_host_mapping_host = "test-host.example.com"
+ssl_server_cert_host_mapping_alt_certificates = ["cert1","cert2"]
+ssl_server_cert_host_mapping_certificate = "test-host.example.com"
+ssl_support_ssl2 = "use_default"
+ssl_support_ssl3 = "use_default"
+ssl_support_tls1 = "use_default"
+ssl_support_tls1_1 = "use_default"
+ssl_support_tls1_2 = "use_default"
+}
+`, virtualServerName)
 }
 
 func testAccBrocadeVTMVirtualServerCreate(virtualServerName string) string {
 	return fmt.Sprintf(`
 resource "brocadevtm_virtual_server" "acctest" {
 name = "%s"
+enabled = true
+listen_on_any = false
+listen_traffic_ips = ["test-traffic-ip-group"]
+pool = "test-pool"
+port = 80
+protocol = "http"
+request_rules = []
+ssl_decrypt = false
+connection_keepalive = false
+connection_keepalive_timeout = 60
+connection_max_client_buffer = 256
+connection_max_server_buffer = 256
+connection_max_transaction_duration = 100
+connection_server_first_banner = "Banner"
+connection_timeout = 60
+ssl_server_cert_default = "abcdefg"
+ssl_server_cert_host_mapping_host = "test-host.example.com"
+ssl_server_cert_host_mapping_alt_certificates = ["cert1","cert2"]
+ssl_server_cert_host_mapping_certificate = "test-host.example.com"
+ssl_support_ssl2 = "use_default"
+ssl_support_ssl3 = "use_default"
+ssl_support_tls1 = "use_default"
+ssl_support_tls1_1 = "use_default"
+ssl_support_tls1_2 = "use_default"
 }
 `, virtualServerName)
 }
