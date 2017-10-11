1479dcbcd14cc2e5153c570b9d9c4b3044c7e848
ppwarwick <paul.warwick88@gmail.com>
2017-10-11 15:50:12 +0100

    Implemented remain pool attributes

--- brocadevtm/util/validation_functions.go
+++ brocadevtm/util/validation_functions.go
@@ -1,6 +1,9 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 // ValidateUnsignedInteger : check integer is unsigned
 func ValidateUnsignedInteger(v interface{}, k string) (ws []string, errors []error) {
@@ -11,10 +14,29 @@ func ValidateUnsignedInteger(v interface{}, k string) (ws []string, errors []err
 	return
 }
 
+// ValidateIP : check valid IP address
+func ValidateIP(v interface{}, k string) (ws []string, errors []error) {
+	ip := v.(string)
+	validateIP := regexp.MustCompile(`^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]$`)
+	if !validateIP.MatchString(ip) {
+		errors = append(errors, fmt.Errorf("%q must be a valid IP. i.e 10.0.0.1", k))
+	}
+	return
+}
+
 // ValidatePortNumber : check port number is valid
 func ValidatePortNumber(v interface{}, k string) (ws []string, errors []error) {
 	if v.(int) < 1 || v.(int) > 65535 {
 		errors = append(errors, fmt.Errorf("Port has to be between 1 and 65535"))
+	}
+	return
+}
+
+// ValidateTCPPort : check TCP port is within range
+func ValidateTCPPort(v interface{}, k string) (ws []string, errors []error) {
+	port := v.(int)
+	if port < 1 || port > 65535 {
+		errors = append(errors, fmt.Errorf("%q must be a valid port number in the range 1 to 65535", k))
 	}
 	return
 }
--- vendor/github.com/sky-uk/go-brocade-vtm/api/pool/pool_model.go
+++ vendor/github.com/sky-uk/go-brocade-vtm/api/pool/pool_model.go
@@ -8,41 +8,86 @@ type Pool struct {
 
 // Properties - General Properties for the pool
 type Properties struct {
-	Basic         Basic         `json:"basic,omitempty"`
-	Connection    Connection    `json:"connection,omitempty"`
-	HTTP          HTTP          `json:"http,omitempty"`
-	LoadBalancing LoadBalancing `json:"load_balancing,omitempty"`
-	Node          Node          `json:"node,omitempty"`
-	Ssl           Ssl           `json:"ssl,omitempty"`
-	TCP           TCP           `json:"tcp,omitempty"`
+	Basic                      Basic                      `json:"basic,omitempty"`
+	AutoScaling                AutoScaling                `json:"auto_scaling,omitempty"`
+	Connection                 Connection                 `json:"connection,omitempty"`
+	DNSAutoScale               DNSAutoScale               `json:"dns_autoscale,omitempty"`
+	FTP                        FTP                        `json:"ftp,omitempty"`
+	HTTP                       HTTP                       `json:"http,omitempty"`
+	KerberosProtocolTransition KerberosProtocolTransition `json:"kerberos_protocol_transition,omitempty"`
+	LoadBalancing              LoadBalancing              `json:"load_balancing,omitempty"`
+	Node                       Node                       `json:"node,omitempty"`
+	SMTP                       SMTP                       `json:"smtp,omitempty"`
+	Ssl                        Ssl                        `json:"ssl,omitempty"`
+	TCP                        TCP                        `json:"tcp,omitempty"`
+	UDP                        UDP                        `json:"udp,omitempty"`
 }
 
 // Basic - main pool definitions
 type Basic struct {
 	BandwidthClass               string       `json:"bandwidth_class,omitempty"`
 	FailurePool                  string       `json:"failure_pool,omitempty"`
-	MaxConnectionAttempts        uint         `json:"max_connection_attempts,omitempty"`
+	MaxConnectionAttempts        *uint        `json:"max_connection_attempts,omitempty"`
 	MaxIdleConnectionsPerNode    uint         `json:"max_idle_connections_pernode,omitempty"`
 	MaxTimeoutConnectionAttempts uint         `json:"max_timed_out_connection_attempts,omitempty"`
 	Monitors                     []string     `json:"monitors,omitempty"`
-	NodeCloseWithReset           *bool        `json:"node_close_with_rst,omitempty"`
+	NodeCloseWithReset           bool         `json:"node_close_with_rst"`
 	NodeConnectionAttempts       uint         `json:"node_connection_attempts,omitempty"`
 	NodeDeleteBehavior           string       `json:"node_delete_behavior,omitempty"`
-	NodeDrainDeleteTimeout       uint         `json:"node_drain_to_delete_timeout"`
+	NodeDrainDeleteTimeout       *uint        `json:"node_drain_to_delete_timeout,omitempty"`
 	NodesTable                   []MemberNode `json:"nodes_table,omitempty"`
 	Note                         string       `json:"note,omitempty"`
-	PassiveMonitoring            *bool        `json:"passive_monitoring,omitempty"`
+	PassiveMonitoring            bool         `json:"passive_monitoring"`
 	PersistenceClass             string       `json:"persistence_class,omitempty"`
-	Transparent                  *bool        `json:"transparent,omitempty"`
+	Transparent                  bool         `json:"transparent"`
+}
+
+// AutoScaling - AutoScaling settings
+type AutoScaling struct {
+	AddNodeDelayTime *uint    `json:"addnode_delaytime,omitempty"`
+	CloudCredentials string   `json:"cloud_credentials,omitempty"`
+	Cluster          string   `json:"cluster,omitempty"`
+	DataCenter       string   `json:"data_center,omitempty"`
+	DataStore        string   `json:"data_store,omitempty"`
+	Enabled          *bool    `json:"enabled,omitempty"`
+	External         *bool    `json:"external,omitempty"`
+	ExtraArgs        string   `json:"extraargs,omitempty"`
+	Hysteresis       *uint    `json:"hysteresis,omitempty"`
+	ImageID          string   `json:"imageid,omitempty"`
+	IPsToUse         string   `json:"ips_to_use,omitempty"`
+	LastNodeIdleTime *uint    `json:"last_node_idle_time,omitempty"`
+	MaxNodes         *uint    `json:"max_nodes,omitempty"`
+	MinNodes         *uint    `json:"min_nodes,omitempty"`
+	Name             string   `json:"name,omitempty,omitempty"`
+	Port             *uint    `json:"port,omitempty,omitempty"`
+	Refractory       *uint    `json:"refractory,omitempty"`
+	ResponseTime     *uint    `json:"response_time,omitempty"`
+	ScaleDownLevel   *uint    `json:"scale_down_level,omitempty"`
+	ScaleUpLevel     *uint    `json:"scale_up_level,omitempty"`
+	SecurityGroupIDs []string `json:"securitygroupids,omitempty"`
+	SizeID           string   `json:"size_id,omitempty"`
+	SubnetIDs        []string `json:"subnetids,omitempty"`
 }
 
 // Connection - Connection setting
 type Connection struct {
-	MaxConnectTime        uint `json:"max_connect_time,omitempty"`
-	MaxConnectionsPerNode uint `json:"max_connections_per_node,omitempty"`
-	MaxQueueSize          uint `json:"max_queue_size,omitempty"`
-	MaxReplyTime          uint `json:"max_reply_time,omitempty"`
-	QueueTimeout          uint `json:"queue_timeout,omitempty"`
+	MaxConnectTime        *uint `json:"max_connect_time,omitempty"`
+	MaxConnectionsPerNode *uint `json:"max_connections_per_node,omitempty"`
+	MaxQueueSize          *uint `json:"max_queue_size,omitempty"`
+	MaxReplyTime          *uint `json:"max_reply_time,omitempty"`
+	QueueTimeout          *uint `json:"queue_timeout,omitempty"`
+}
+
+// DNSAutoScale - DNSAutoScale settings
+type DNSAutoScale struct {
+	Enabled   *bool    `json:"enabled,omitempty"`
+	Hostnames []string `json:"hostnames,omitempty"`
+	Port      *uint    `json:"port,omitempty"`
+}
+
+// FTP - FTP settings
+type FTP struct {
+	SupportRFC2428 *bool `json:"support_rfc_2428,omitempty"`
 }
 
 // HTTP - http settings
@@ -51,30 +96,47 @@ type HTTP struct {
 	HTTPKeepAliveNonIdempotent *bool `json:"keepalive_non_idempotent,omitempty"`
 }
 
+// KerberosProtocolTransition - KerberosProtocolTransition settings
+type KerberosProtocolTransition struct {
+	Principal string `json:"principal,omitempty"`
+	Target    string `json:"target,omitempty"`
+}
+
 // LoadBalancing - Pool Load balancing settings
 type LoadBalancing struct {
 	Algorithm       string `json:"algorithm,omitempty"`
 	PriorityEnabled *bool  `json:"priority_enabled,omitempty"`
-	PriorityNodes   uint   `json:"priority_nodes,omitempty"`
+	PriorityNodes   *uint  `json:"priority_nodes,omitempty"`
 }
 
 // Node - Node Specific settings
 type Node struct {
 	CloseOnDeath  *bool `json:"close_on_death,omitempty"`
-	RetryFailTime int   `json:"retry_fail_time,omitempty"`
+	RetryFailTime *uint `json:"retry_fail_time,omitempty"`
+}
+
+// SMTP - SMTP settings
+type SMTP struct {
+	SendSTARTTLS *bool `json:"send_starttls,omitempty"`
 }
 
 // Ssl - SSL related settings
 type Ssl struct {
 	ClientAuth          *bool    `json:"client_auth,omitempty"`
 	CommonNameMatch     []string `json:"common_name_match,omitempty"`
-	ElipticCurves       []string `json:"eliptic_curves,omitempty"`
-	Enabled             *bool    `json:"enabled,omitempty"`
+	EllipticCurves      []string `json:"elliptic_curves,omitempty"`
+	Enable              *bool    `json:"enable,omitempty"`
 	Enhance             *bool    `json:"enhance,omitempty"`
 	SendCloseAlerts     *bool    `json:"send_close_alerts,omitempty"`
 	ServerName          *bool    `json:"server_name,omitempty"`
 	SignatureAlgorithms string   `json:"signature_algorithms,omitempty"`
 	SslCiphers          string   `json:"ssl_ciphers,omitempty"`
+	SSLSupportSSL2      string   `json:"ssl_support_ssl2,omitempty"`
+	SSLSupportSSL3      string   `json:"ssl_support_ssl3,omitempty"`
+	SSLSupportTLS1      string   `json:"ssl_support_tls1,omitempty"`
+	SSLSupportTLS1_1    string   `json:"ssl_support_tls1_1,omitempty"`
+	SSLSupportTLS1_2    string   `json:"ssl_support_tls1_2,omitempty"`
+	StrictVerify        *bool    `json:"strict_verify,omitempty"`
 }
 
 // TCP - tcp setting
@@ -82,12 +144,20 @@ type TCP struct {
 	Nagle *bool `json:"nagle,omitempty"`
 }
 
+// UDP - UDP setting
+type UDP struct {
+	AcceptFrom      string `json:"accept_from,omitempty"`
+	AcceptFromMask  string `json:"accept_from_mask,omitempty"`
+	ResponseTimeout *uint  `json:"response_timeout,omitempty"`
+}
+
 // MemberNode - Pool membership details / node /state / weight
 type MemberNode struct {
-	Node     string `json:"node"`
-	Priority int    `json:"priority"`
-	State    string `json:"state"`
-	Weight   int    `json:"weight"`
+	Node     string `json:"node,omitempty"`
+	Priority *uint  `json:"priority,omitempty"`
+	State    string `json:"state,omitempty"`
+	Weight   *int   `json:"weight,omitempty"`
+	SourceIP string `json:"source_ip,omitempty"`
 }
 
 // LBPoolList - Used to return all pools
