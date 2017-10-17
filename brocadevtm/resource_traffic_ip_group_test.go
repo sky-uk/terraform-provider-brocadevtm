42d55140e5aca2ca3a0217bc1cae7f65c665688e
Paul Warwick <paul.warwick@sky.uk>
2017-10-17 12:19:38 +0100

    Added new attributes to traffic IP group

--- brocadevtm/resource_traffic_ip_group_test.go
+++ brocadevtm/resource_traffic_ip_group_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/sky-uk/go-brocade-vtm/api/traffic_ip_group"
 	"github.com/sky-uk/go-rest-api"
+	"github.com/sky-uk/terraform-provider-brocadevtm/brocadevtm/util"
 	"regexp"
 	"testing"
 )
@@ -16,6 +17,11 @@ func TestAccBrocadeVTMTrafficIpGroupBasic(t *testing.T) {
 	randomInt := acctest.RandInt()
 	trafficIPGroupName := fmt.Sprintf("acctest_brocadevtm_traffic_ip_group-%d", randomInt)
 	trafficIPGroupResourceName := "brocadevtm_traffic_ip_group.acctest"
+
+	ipMappingIPPattern := regexp.MustCompile(`ip_mapping\.[0-9]+\.ip`)
+	ipMappingTMPattern := regexp.MustCompile(`ip_mapping\.[0-9]+\.traffic_manager`)
+	ipAddressesPattern := regexp.MustCompile(`ipaddresses\.[0-9]+`)
+	slavesPattern := regexp.MustCompile(`slaves\.[0-9]+`)
 
 	fmt.Printf("\n\nTraffic IP Group is %s.\n\n", trafficIPGroupName)
 
@@ -63,8 +69,25 @@ func TestAccBrocadeVTMTrafficIpGroupBasic(t *testing.T) {
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "name", trafficIPGroupName),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "enabled", "true"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "hash_source_port", "true"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_assignment_mode", "alphabetic"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_mapping.#", "1"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingIPPattern, "192.168.34.56"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingTMPattern, "10.93.59.24"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ipaddresses.#", "1"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipAddressesPattern, "192.168.100.10"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "keeptogether", "true"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "location", "10"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "mode", "singlehosted"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "multicast", "232.123.23.45"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "note", "Acceptance test - create"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_metric_base", "5"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_passive_metric_offset", "2"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_metric_base", "7"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_passive_metric_offset", "3"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_protocols", "ospf"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "slaves.#", "2"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.45"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.46"),
 				),
 			},
 			{
@@ -74,8 +97,29 @@ func TestAccBrocadeVTMTrafficIpGroupBasic(t *testing.T) {
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "name", trafficIPGroupName),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "enabled", "false"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "hash_source_port", "false"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_assignment_mode", "balanced"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_mapping.#", "2"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingIPPattern, "192.168.34.56"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingIPPattern, "192.168.34.64"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingTMPattern, "10.93.59.24"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingTMPattern, "10.93.59.24"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ipaddresses.#", "2"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipAddressesPattern, "192.168.100.11"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipAddressesPattern, "192.168.100.12"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "keeptogether", "false"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "location", "12"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "mode", "multihosted"),
-					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "multicast", "232.123.23.43"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "multicast", "232.123.23.143"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "note", "Acceptance test - update"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_metric_base", "15"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_passive_metric_offset", "3"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_metric_base", "17"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_passive_metric_offset", "5"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_protocols", "bgp"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "slaves.#", "3"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.47"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.46"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.45"),
 				),
 			},
 			{
@@ -85,8 +129,27 @@ func TestAccBrocadeVTMTrafficIpGroupBasic(t *testing.T) {
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "name", trafficIPGroupName),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "enabled", "true"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "hash_source_port", "true"),
-					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "mode", "multihosted"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_assignment_mode", "alphabetic"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ip_mapping.#", "2"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingIPPattern, "192.168.34.64"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingIPPattern, "192.168.34.56"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingTMPattern, "10.93.59.24"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipMappingTMPattern, "10.93.59.24"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "ipaddresses.#", "1"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, ipAddressesPattern, "192.168.100.12"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "keeptogether", "true"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "location", "5"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "mode", "singlehosted"),
 					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "multicast", "232.123.23.48"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "note", "Acceptance test - update 2"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_metric_base", "12"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_bgp_passive_metric_offset", "4"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_metric_base", "14"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_ospfv2_passive_metric_offset", "4"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "rhi_protocols", "ospf"),
+					resource.TestCheckResourceAttr(trafficIPGroupResourceName, "slaves.#", "2"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.46"),
+					util.AccTestCheckValueInKeyPattern(trafficIPGroupResourceName, slavesPattern, "192.168.34.45"),
 				),
 			},
 		},
@@ -213,9 +276,25 @@ resource "brocadevtm_traffic_ip_group" "acctest" {
   name = "%s"
   enabled = true
   hash_source_port = true
+  ip_assignment_mode = "alphabetic"
+  ip_mapping = [
+    {
+      ip = "192.168.34.56"
+      traffic_manager = "10.93.59.24"
+    },
+  ]
   ipaddresses = ["192.168.100.10"]
+  keeptogether = true
+  location = 10
   mode = "singlehosted"
   multicast = "232.123.23.45"
+  note = "Acceptance test - create"
+  rhi_bgp_metric_base = 5
+  rhi_bgp_passive_metric_offset = 2
+  rhi_ospfv2_metric_base = 7
+  rhi_ospfv2_passive_metric_offset = 3
+  rhi_protocols = "ospf"
+  slaves = [ "192.168.34.45", "192.168.34.46" ]
 }
 `, trafficIPGroupName)
 }
@@ -226,9 +305,29 @@ resource "brocadevtm_traffic_ip_group" "acctest" {
   name = "%s"
   enabled = false
   hash_source_port = false
-  ipaddresses = ["192.168.100.11"]
+  ip_assignment_mode = "balanced"
+  ip_mapping = [
+    {
+      ip = "192.168.34.56"
+      traffic_manager = "10.93.59.24"
+    },
+    {
+      ip = "192.168.34.64"
+      traffic_manager = "10.93.59.24"
+    },
+  ]
+  ipaddresses = ["192.168.100.11", "192.168.100.12"]
+  keeptogether = false
+  location = 12
   mode = "multihosted"
-  multicast = "232.123.23.43"
+  multicast = "232.123.23.143"
+  note = "Acceptance test - update"
+  rhi_bgp_metric_base = 15
+  rhi_bgp_passive_metric_offset = 3
+  rhi_ospfv2_metric_base = 17
+  rhi_ospfv2_passive_metric_offset = 5
+  rhi_protocols = "bgp"
+  slaves = [ "192.168.34.47", "192.168.34.46", "192.168.34.45" ]
 }
 `, trafficIPGroupName)
 }
@@ -239,10 +338,30 @@ resource "brocadevtm_traffic_ip_group" "acctest" {
   name = "%s"
   enabled = true
   hash_source_port = true
+  ip_assignment_mode = "alphabetic"
+  ip_mapping = [
+    {
+      ip = "192.168.34.64"
+      traffic_manager = "10.93.59.24"
+    },
+    {
+      ip = "192.168.34.56"
+      traffic_manager = "10.93.59.24"
+    },
+  ]
   ipaddresses = ["192.168.100.12"]
+  keeptogether = true
+  location = 5
   machines = [ "192.168.10.11", "10.93.59.24" ]
-  mode = "multihosted"
+  mode = "singlehosted"
   multicast = "232.123.23.48"
+  note = "Acceptance test - update 2"
+  rhi_bgp_metric_base = 12
+  rhi_bgp_passive_metric_offset = 4
+  rhi_ospfv2_metric_base = 14
+  rhi_ospfv2_passive_metric_offset = 4
+  rhi_protocols = "ospf"
+  slaves = [ "192.168.34.46", "192.168.34.45" ]
 }
 `, trafficIPGroupName)
 }
