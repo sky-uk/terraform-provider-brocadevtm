a2c85f3d14222f605b833a8bc7eb4ef152a60101
Paul Warwick <paul.warwick@sky.uk>
2017-11-21 17:44:27 +0000

    Refactored CRUD to use maps

--- brocadevtm/util/helper_functions.go
+++ brocadevtm/util/helper_functions.go
@@ -92,3 +92,69 @@ func SetSimpleAttributesFromMap(d *schema.ResourceData, mapItem map[string]inter
 		d.Set(attributeName, mapItem[item])
 	}
 }
+
+// BuildListMaps : builds a list of maps from a list of interfaces using a list of attributes
+func BuildListMaps(itemList *schema.Set, attributeNames []string) ([]map[string]interface{}, error) {
+
+	listOfMaps := make([]map[string]interface{}, 0)
+
+	for _, item := range itemList.List() {
+
+		definedItem := item.(map[string]interface{})
+		newMap := make(map[string]interface{})
+
+		for _, attributeName := range attributeNames {
+
+			if attributeValue, ok := definedItem[attributeName]; ok {
+				switch attributeValue.(type) {
+				case bool:
+					newMap[attributeName] = attributeValue.(bool)
+				case string:
+					newMap[attributeName] = attributeValue.(string)
+				case int:
+					newMap[attributeName] = attributeValue.(int)
+				case []interface{}:
+					newMap[attributeName] = attributeValue
+				case *schema.Set:
+					newMap[attributeName] = attributeValue.(*schema.Set).List()
+				case map[string]interface{}:
+					newMap[attributeName] = attributeValue.(map[string]interface{})
+				case []map[string]interface{}:
+					newMap[attributeName] = attributeValue.([]map[string]interface{})
+				default:
+					return listOfMaps, fmt.Errorf("util.BuildListMaps doesn't understand type for %+v", attributeValue)
+				}
+			}
+		}
+		listOfMaps = append(listOfMaps, newMap)
+	}
+	if len(listOfMaps) == 0 {
+		emptyMap := make(map[string]interface{})
+		listOfMaps = append(listOfMaps, emptyMap)
+	}
+	return listOfMaps, nil
+}
+
+// BuildReadListMaps : used by a read to build a list of maps which contain bools, strings, ints, float64s and lists of strings
+func BuildReadListMaps(inputMap map[string]interface{}, attributeName string) (map[string]interface{}, error) {
+
+	builtMap := make(map[string]interface{})
+
+	for key, value := range inputMap {
+
+		switch value.(type) {
+		case bool:
+			builtMap[key] = value.(bool)
+		case string:
+			builtMap[key] = value.(string)
+		case float64:
+			builtMap[key] = value.(float64)
+		// []interface{} only configured / tested for a list of strings
+		case []interface{}:
+			builtMap[key] = schema.NewSet(schema.HashString, value.([]interface{}))
+		default:
+			return builtMap, fmt.Errorf("util.BuildReadListMaps doesn't understand type for %+v", value)
+		}
+	}
+	return builtMap, nil
+}
