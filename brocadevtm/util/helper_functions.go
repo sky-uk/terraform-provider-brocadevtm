6e36d597ee596f1cce7b786e84ff7c26d21a3381
Paul Warwick <paul.warwick@sky.uk>
2017-11-23 18:03:28 +0000

    Removed unused helper functions

--- brocadevtm/util/helper_functions.go
+++ brocadevtm/util/helper_functions.go
@@ -7,24 +7,6 @@ import (
 	"reflect"
 )
 
-// BuildStringArrayFromInterface : take an interface and convert it into an array of strings
-func BuildStringArrayFromInterface(strings interface{}) []string {
-	stringList := make([]string, len(strings.([]interface{})))
-	for idx, stringValue := range strings.([]interface{}) {
-		stringList[idx] = stringValue.(string)
-	}
-	return stringList
-}
-
-// BuildStringListFromSet : take an interface and convert it into an array of strings
-func BuildStringListFromSet(strings *schema.Set) []string {
-	stringList := make([]string, 0)
-	for _, stringValue := range strings.List() {
-		stringList = append(stringList, stringValue.(string))
-	}
-	return stringList
-}
-
 // AddSimpleGetAttributesToMap : wrapper for d.Get
 func AddSimpleGetAttributesToMap(d *schema.ResourceData, mapItem map[string]interface{}, attributeNamePrefix string, attributeNames []string) map[string]interface{} {
 
