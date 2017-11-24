d9fe6b8dca7bcb1ed4b4c654d65241ad660ad810
Paul Warwick <paul.warwick@sky.uk>
2017-11-24 17:24:56 +0000

    Refactored monitor and removed unused helper functions

--- brocadevtm/util/helper_functions.go
+++ brocadevtm/util/helper_functions.go
@@ -7,47 +7,6 @@ import (
 	"reflect"
 )
 
-// AddSimpleGetAttributesToMap : wrapper for d.Get
-func AddSimpleGetAttributesToMap(d *schema.ResourceData, mapItem map[string]interface{}, attributeNamePrefix string, attributeNames []string) map[string]interface{} {
-
-	for _, item := range attributeNames {
-		attributeName := fmt.Sprintf("%s%s", attributeNamePrefix, item)
-		attributeValue := d.Get(attributeName)
-		switch attributeValue.(type) {
-		case bool:
-			mapItem[item] = attributeValue.(bool)
-		case string:
-			mapItem[item] = attributeValue.(string)
-		case int:
-			mapItem[item] = attributeValue.(int)
-		case *schema.Set:
-			mapItem[item] = attributeValue.(*schema.Set).List()
-		default:
-		}
-	}
-	return mapItem
-}
-
-// AddSimpleGetOkAttributesToMap : wrapper for d.GetOk
-func AddSimpleGetOkAttributesToMap(d *schema.ResourceData, mapItem map[string]interface{}, attributeNamePrefix string, attributeNames []string) map[string]interface{} {
-
-	for _, item := range attributeNames {
-		attributeName := fmt.Sprintf("%s%s", attributeNamePrefix, item)
-		if attributeValue, ok := d.GetOk(attributeName); ok {
-			switch attributeValue.(type) {
-			case bool:
-				mapItem[item] = attributeValue.(bool)
-			case string:
-				mapItem[item] = attributeValue.(string)
-			case int:
-				mapItem[item] = attributeValue.(int)
-			default:
-			}
-		}
-	}
-	return mapItem
-}
-
 // AddChangedSimpleAttributesToMap : wrapper for d.HasChange & d.Get
 func AddChangedSimpleAttributesToMap(d *schema.ResourceData, mapItem map[string]interface{}, attributeNamePrefix string, attributeNames []string) map[string]interface{} {
 
@@ -70,57 +29,6 @@ func AddChangedSimpleAttributesToMap(d *schema.ResourceData, mapItem map[string]
 		}
 	}
 	return mapItem
-}
-
-// SetSimpleAttributesFromMap : wrapper for d.Set
-func SetSimpleAttributesFromMap(d *schema.ResourceData, mapItem map[string]interface{}, attributeNamePrefix string, attributeNames []string) {
-
-	for _, item := range attributeNames {
-		attributeName := fmt.Sprintf("%s%s", attributeNamePrefix, item)
-		d.Set(attributeName, mapItem[item])
-	}
-}
-
-// BuildListMaps : builds a list of maps from a list of interfaces using a list of attributes
-func BuildListMaps(itemList *schema.Set, attributeNames []string) ([]map[string]interface{}, error) {
-
-	listOfMaps := make([]map[string]interface{}, 0)
-
-	for _, item := range itemList.List() {
-
-		definedItem := item.(map[string]interface{})
-		newMap := make(map[string]interface{})
-
-		for _, attributeName := range attributeNames {
-
-			if attributeValue, ok := definedItem[attributeName]; ok {
-				switch attributeValue.(type) {
-				case bool:
-					newMap[attributeName] = attributeValue.(bool)
-				case string:
-					newMap[attributeName] = attributeValue.(string)
-				case int:
-					newMap[attributeName] = attributeValue.(int)
-				case []interface{}:
-					newMap[attributeName] = attributeValue
-				case *schema.Set:
-					newMap[attributeName] = attributeValue.(*schema.Set).List()
-				case map[string]interface{}:
-					newMap[attributeName] = attributeValue.(map[string]interface{})
-				case []map[string]interface{}:
-					newMap[attributeName] = attributeValue.([]map[string]interface{})
-				default:
-					return listOfMaps, fmt.Errorf("util.BuildListMaps doesn't understand type for %+v", attributeValue)
-				}
-			}
-		}
-		listOfMaps = append(listOfMaps, newMap)
-	}
-	if len(listOfMaps) == 0 {
-		emptyMap := make(map[string]interface{})
-		listOfMaps = append(listOfMaps, emptyMap)
-	}
-	return listOfMaps, nil
 }
 
 // BuildReadMap : used by a read to build a list of maps which contain bools, strings, ints, float64s and lists of strings
